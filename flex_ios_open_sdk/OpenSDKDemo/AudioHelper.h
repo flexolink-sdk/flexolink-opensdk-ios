7ae0a18eac32aefecb1af5ccf3effdcc85406884
linpeiwei <775486031@qq.com>
2024-05-09 09:48:47 +0800

    代码上传
    This reverts commit 39ac5cac0ee98475b86905c5a9fe083ec35f76a4.

---
+++ flex_ios_open_sdk/OpenSDKDemo/AudioHelper.h
@@ -0,0 +1,18 @@
+//
+//  AudioHelper.h
+//  OpenSDKDemoDemo
+//
+//  Created by frank on 2024/3/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AudioHelper : NSObject
+
+- (void) playerWithFilePath:(NSString *) filePath;
+
+@end
+
+NS_ASSUME_NONNULL_END
